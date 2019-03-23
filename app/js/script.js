ed5eed6291affae298933f98c291bc8d870e2abd
Denis Toropin <denis.toropin@yandex.ru>
2019-03-23 13:04:22 +0300

    add js

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,35 @@
 (function () {
+	
+	var _elements = $('.card');
 
 	var init = function () {
-		console.log('It is myModule other!');
 		_setUpListners();
 	};
 
 	var _setUpListners = function () {
-		// прослушка событий
+		$('.courses__block').on('click', '.card__input', _blockSlideUp);
 	};
 
+	var _blockSlideUp = function (e) {
+		var card = $(e.target).parents('.card');
+		card.find('.card__checkbox').css('background', '#f6f7f2');
+		card.addClass('active');
+		setTimeout(function () {
+			card.fadeOut();
+		}, 1000);
+		setTimeout(function () {
+			_showComplete();
+		}, 1500);
+	};
+
+	var _showComplete = function () {
+		var show = true;
+		$.each(_elements, function(i, item) {
+			if($(item).is(':visible')) show = false;
+		});
+		// console.log(show);
+		show ? $('.complete').show() : false;
+	};
 
 	return init();
 })();
